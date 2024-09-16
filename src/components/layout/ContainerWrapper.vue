2c9affdd5b9c0f597eac0b589c3bc1c30935bf7f
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-09-16 12:09:01 -0500

    FIX: bg color

--- src/components/layout/ContainerWrapper.vue
+++ src/components/layout/ContainerWrapper.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container-fluid m-0 p-0 min-vh-100 text-bg-primary">
+  <div class="container-fluid m-0 p-0 min-vh-100 text-bg-gray-100">
     <div class="container-xxl m-auto d-flex justify-content-center align-items-center">
       <slot name="content" />
     </div>
