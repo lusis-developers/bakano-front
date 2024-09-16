5e46faa3cc76839a2a7d6a996c6943d29c383808
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-09-16 11:35:05 -0500

    ADD: general container, ADD: login view, REFACTOR: router

---
+++ src/components/layout/ContainerWrapper.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container-fluid m-0 p-0 min-vh-100 text-bg-primary">
+    <div class="container-xxl m-auto d-flex justify-content-center align-items-center">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ContainerView from '@/components/layout/ContainerView.vue'
+</script>
+
+<template>
+  <div class="login">
+    <ContainerView> </ContainerView>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
