9e1f81af60b5a13342fc5ce7e71c2a6243d6e5d6
Diego Reyes <diegorele13@gmail.com>
2024-09-17 12:38:47 -0500

    two example wizard

--- src/views/wizard/components/FirstStep.vue
+++ src/views/wizard/components/FirstStep.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const emit = defineEmits(['next']);
+
+function emitNext(): void {
+  emit('next');
+}
+</script>
+
 <template>
   <div>
     <h1>Step 1</h1>
     <p>Step 1 content</p>
+    <button @click="emitNext">Next</button>
   </div>
 </template>
--- src/views/wizard/components/SecondStep.vue
+++ src/views/wizard/components/SecondStep.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const emit = defineEmits(['next']);
+
+function emitNext(): void {
+  emit('next');
+}
+</script>
+
 <template>
   <div>
     <h1>Step 2</h1>
     <p>Step 2 content</p>
+    <button @click="emitNext">Next</button>
   </div>
 </template>
