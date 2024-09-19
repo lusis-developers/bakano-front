9dd315f28fed0b29f0ba24c32eec68c0fc30e33e
Diego Reyes <diegorele13@gmail.com>
2024-09-19 14:12:10 -0500

    add: menu items and interfaces

---
+++ src/interfaces/Layout/LinkTypes.interface.ts
@@ -0,0 +1,5 @@
+export interface Navigation {
+  name: string
+  link: string
+  icon?: string
+}
---
+++ src/utils/menuItems.utils.ts
@@ -0,0 +1,15 @@
+import type { Navigation } from '@/interfaces/Layout/LinkTypes.interface'
+
+export const menuItems: Navigation[] = [
+  {
+    name: 'Agregar marca',
+    link: '#',
+    icon: 'bi bi-plus-lg'
+  }
+]
+
+export const sidebarItems: Navigation[] = [
+  { name: 'Resumen', icon: 'bi bi-card-checklist', link: '#' },
+  { name: 'Generar planificación', icon: 'bi bi-journal-richtext', link: '#' },
+  { name: 'Generar post específico', icon: 'bi bi-file-post', link: '#' }
+]
