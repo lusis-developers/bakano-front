c228ff1d171f58833a0a66bfbd8043ba7a8dd407
Diego Reyes <diegorele13@gmail.com>
2024-09-19 16:23:40 -0500

    add: interfaces

--- src/interfaces/Layout/BrandsTypes.interface.ts
+++ src/interfaces/Layout/BrandsTypes.interface.ts
@@ -1,13 +1,13 @@
 export interface socialMedia {
-  name: string;
-  link: string;
+  name: string
+  link: string
 }
 
 export interface Brand {
-  id: number;
-  name: string;
-  logo: string;
-  socialMedia: socialMedia[];
-  isLoading: boolean;
-  error: null;
+  id: number
+  name: string
+  logo: string
+  socialMedia: socialMedia[]
+  isLoading: boolean
+  error: null
 }
