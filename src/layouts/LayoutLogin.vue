5c46b8db1fa980b62d994b82c89b1f2df86c7dae
Diego Reyes <diegorele13@gmail.com>
2024-09-19 16:46:33 -0500

    add selected item menu

--- src/layouts/LayoutLogin.vue
+++ src/layouts/LayoutLogin.vue
@@ -23,6 +23,9 @@ function getItemClasses(index: number): { [key: string]: boolean } {
 function updateSidebarState(): void {
   isSidebarExpanded.value = window.innerWidth >= 768
 }
+function handleBrandSelection(brand: Brand): void {
+  brandStore.setSelectedBrand(brand)
+}
 function getMenuItems(brands: Brand[]): MenuDropdownItem[] {
   return brands.map((brand) => ({
     ...brand,
@@ -82,15 +85,27 @@ onBeforeUnmount(() => {
               <slot name="header"></slot>
             </div>
             <div class="d-flex align-items-center gap-3">
-              <DropdownMenu :menuOptions="menuOptions" :menuItems="getMenuItems(brandStore.brands)">
+              <DropdownMenu
+                :menuOptions="menuOptions"
+                :menuItems="getMenuItems(brandStore.brands)"
+                @item-click="handleBrandSelection"
+              >
                 <template #button-content>
                   <img
-                    src="https://i.pinimg.com/236x/22/09/02/220902e0b406bbd28afccd44a3551b1e.jpg"
-                    alt="Profile picture"
+                    :src="
+                      brandStore.selectedBrand?.logo ||
+                      'https://i.pinimg.com/236x/22/09/02/220902e0b406bbd28afccd44a3551b1e.jpg'
+                    "
+                    :alt="
+                      brandStore.selectedBrand?.name ||
+                      'https://i.pinimg.com/236x/22/09/02/220902e0b406bbd28afccd44a3551b1e.jpg'
+                    "
                     class="rounded-2"
                     style="width: 40px; height: 40px"
                   />
-                  <p class="m-0 d-none d-md-inline">Boxitrip</p>
+                  <p class="m-0 d-none d-md-inline">
+                    {{ brandStore.selectedBrand?.name || 'Seleccionar marca' }}
+                  </p>
                 </template>
               </DropdownMenu>
               <button class="btn btn btn-light p-2 px-3">
@@ -114,7 +129,6 @@ onBeforeUnmount(() => {
   transition: width 0.3s;
   width: 80px;
 }
-
 .col-3 {
   transition: width 0.3s;
   width: 250px;
