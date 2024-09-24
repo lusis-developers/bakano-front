fcf47a1a81f49ee72b71be19a56fe25a1d13f13e
Diego Reyes <diegorele13@gmail.com>
2024-09-24 15:43:08 -0500

    add: user enum

---
+++ src/enum/user.enum.ts
@@ -0,0 +1,41 @@
+export enum SubscriptionPlan {
+  FREE = 'free',
+  MONTHLY = 'monthly',
+  YEARLY = 'yearly'
+}
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  SUSPENDED = 'suspended'
+}
+
+export enum JobDescription {
+  FREELANCER = 'freelancer',
+  MARKETING_AGENCY = 'marketing_agency',
+  COMPANY = 'company',
+  CONTENT_CREATOR = 'content_creator'
+}
+
+export enum BakanoUsageCount {
+  INDIVIDUAL = 'individual',
+  GROUP = 'group'
+}
+
+export enum BakanoUsageType {
+  SCHEDULE_POSTS = 'schedule_posts',
+  CONTENT_CREATION = 'content_creation'
+}
+
+export enum FoundUsBy {
+  TIK_TOK = 'tik_tok',
+  FACEBOOK = 'facebook',
+  INSTAGRAM = 'instagram',
+  GOOGLE = 'google'
+}
+
+export enum UserGender {
+  MALE = 'male',
+  FEMALE = 'female',
+  PREFER_NOT_SAY = 'prefer not say'
+}
