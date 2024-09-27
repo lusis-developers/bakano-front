21334f1a2fa9f0cef9edd0d03a6265ab71bc509f
Diego Reyes <diegorele13@gmail.com>
2024-09-27 12:26:41 -0500

    add: wizard selections

---
+++ src/utils/wizardSelections.utils.ts
@@ -0,0 +1,41 @@
+import { BakanoUsageCount, BakanoUsageType, FoundUsBy, JobDescription } from '@/enum/user.enum'
+
+import type { SelectOption } from '@/interfaces/components/input/SelectInput.interface'
+
+export const usageCountOptions: SelectOption[] = [
+  { value: '', label: 'Seleccione', icon: 'bi bi-people-fill' },
+  { value: BakanoUsageCount.INDIVIDUAL, label: 'Individual', icon: 'bi bi-people-fill' },
+  { value: BakanoUsageCount.GROUP, label: 'Grupo', icon: 'bi bi-people-fill' }
+]
+
+export const usageTypeOptions: SelectOption[] = [
+  { value: '', label: 'Seleccione', icon: 'bi bi-gear-fill' },
+  {
+    value: BakanoUsageType.SCHEDULE_POSTS,
+    label: 'Programar Publicaciones',
+    icon: 'bi bi-gear-fill'
+  },
+  {
+    value: BakanoUsageType.CONTENT_CREATION,
+    label: 'Creación de Contenido',
+    icon: 'bi bi-gear-fill'
+  }
+]
+
+export const foundUsByOptions: SelectOption[] = [
+  { value: '', label: 'Seleccione', icon: 'bi bi-search' },
+  { value: FoundUsBy.TIK_TOK, label: 'TikTok' },
+  { value: FoundUsBy.FACEBOOK, label: 'Facebook' },
+  { value: FoundUsBy.INSTAGRAM, label: 'Instagram' },
+  { value: FoundUsBy.GOOGLE, label: 'Google' },
+  { value: FoundUsBy.WORD_OF_MOUTH, label: 'WOM (Amigo, compañero, hijo)' }
+]
+
+export const jobDescriptionOptions: SelectOption[] = [
+  { value: '', label: 'Seleccione' },
+  { value: JobDescription.FREELANCER, label: 'Freelancer' },
+  { value: JobDescription.MARKETING_AGENCY, label: 'Agencia de Marketing' },
+  { value: JobDescription.COMPANY, label: 'Empresa' },
+  { value: JobDescription.CONTENT_CREATOR, label: 'Creador de Contenido' },
+  { value: JobDescription.OTHER, label: 'Otro' }
+]
