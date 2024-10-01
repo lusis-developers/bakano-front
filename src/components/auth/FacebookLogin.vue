27b410ab96478f2fd9340da6c66715009be60d95
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-10-01 15:28:28 -0500

    FEAT: login permissions granted

--- src/components/auth/FacebookLogin.vue
+++ src/components/auth/FacebookLogin.vue
@@ -4,37 +4,55 @@ import { onMounted } from 'vue'
 
 const authStore = useAuthStore()
 
-function initFacebookSDK() {
-  // Inicializar el SDK de Facebook
-  window.fbAsyncInit = function () {
-    window.FB.init({
-      appId: import.meta.env.VITE_FACEBOOK_ID, // Reemplaza con tu App ID de Facebook
-      cookie: true,
-      xfbml: true,
-      version: 'v20.0' // Versión de la API
-    })
-    window.FB.AppEvents.logPageView() // Registro de eventos
-  }
+async function initFacebookSDK() {
+  try {
+    // Inicializar el SDK de Facebook
+    console.log('estamos encima del init interno')
+    console.log(import.meta.env.VITE_FACEBOOK_ID)
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: import.meta.env.VITE_FACEBOOK_ID, // Reemplaza con tu App ID de Facebook
+        cookie: true,
+        xfbml: true,
+        version: 'v20.0' // Versión de la API
+      })
+      window.FB.AppEvents.logPageView() // Registro de eventos
+    }
+    console.log('estamos debajo del init interno')
 
-  // Cargar el SDK de Facebook de manera asincrónica
-  ;(function (d, s, id) {
-    const fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) {
-      return
-    }
-    const js = d.createElement(s) as HTMLScriptElement
-    js.id = id
-    js.src = 'https://connect.facebook.net/en_US/sdk.js'
-    if (fjs && fjs.parentNode) {
-      fjs.parentNode.insertBefore(js, fjs)
-    }
-  })(document, 'script', 'facebook-jssdk')
+    // Cargar el SDK de Facebook de manera asincrónica
+    ;(function (d, s, id) {
+      const fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {
+        return
+      }
+      const js = d.createElement(s) as HTMLScriptElement
+      js.id = id
+      js.src = 'https://connect.facebook.net/en_US/sdk.js'
+      if (fjs && fjs.parentNode) {
+        fjs.parentNode.insertBefore(js, fjs)
+      }
+    })(document, 'script', 'facebook-jssdk')
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 // Función para manejar el login con Facebook
 function loginWithFacebook() {
   window.FB.getLoginStatus((response: any) => {
-    statusChangeCallback(response)
+    if (response.status === 'connected') {
+      statusChangeCallback(response)
+    } else {
+      window.FB.login(
+        (loginResponse: any) => {
+          statusChangeCallback(loginResponse)
+        },
+        {
+          scope: 'ads_management,ads_read,pages_read_engagement,business_management'
+        }
+      )
+    }
   })
 }
 
---
+++ src/views/TemporalView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FacebookLogin from '@/components/auth/FacebookLogin.vue'
+</script>
+
+<template>
+  <div>
+    <FacebookLogin />
+  </div>
+</template>
