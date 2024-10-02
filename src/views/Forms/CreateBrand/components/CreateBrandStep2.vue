cd7c04674dcae844b0f31a052b25f55af212f30c
Diego Reyes <diegorele13@gmail.com>
2024-10-02 12:31:44 -0500

    add: files

--- src/views/Forms/CreateBrand/components/CreateBrandStep2.vue
+++ src/views/Forms/CreateBrand/components/CreateBrandStep2.vue
@@ -1,25 +1,63 @@
 <script setup>
 import { ref } from 'vue'
 import MultipleSelectInput from '@/components/input/multipleSelectInput.vue'
+import { TargetBrandGender } from '@/enum/brand.enum'
+import FloatInput from '@/components/input/FloatInput.vue'
 
 const selectedAgeRanges = ref([])
+const selectedGenders = ref([])
+const publicPreferences = ref('')
 
-const ageRanges = ['18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75 o más']
+const ageRanges = [
+  '18-24',
+  '25-34',
+  '35-44',
+  '45-54',
+  '55-64',
+  '65-74',
+  '75 o más',
+  'No estoy seguro'
+]
 
-const handleSubmit = () => {
+const genders = [
+  { value: TargetBrandGender.MALE, label: 'Masculino' },
+  { value: TargetBrandGender.FEMALE, label: 'Femenino' },
+  { value: TargetBrandGender.BOTH, label: 'Ambos' },
+  { value: TargetBrandGender.NOT_SURE, label: 'No estoy seguro' }
+]
+
+function handleSubmit () {
   console.log('Rangos de edad seleccionados:', selectedAgeRanges.value)
 }
 </script>
 
 <template>
   <div>
-    <h5>Audiencia objetivo</h5>
+    <h5>Configuración de audiencia</h5>
     <div class="mb-3">
       <MultipleSelectInput
         v-model="selectedAgeRanges"
         :options="ageRanges"
-        label="Rangos de Edad"
+        label="Selecciona tu público objetivo"
         id="ageRangeSelector"
+      />
+      <hr class="my-4" />
+      <MultipleSelectInput
+        v-model="selectedGenders"
+        :options="genders.map((gender) => gender.label)"
+        label="Selecciona los generes objetivo que crees que les puede interesar tu producto"
+        id="ageRangeSelector"
+      />
+      <hr class="my-4" />
+      <h6>
+        Escribe lo que consideres le guste a tu público objetivo
+      </h6>
+      <FloatInput
+        label="Preferencias de tu público"
+        inputId="brandName"
+        placeholder="Escribe lo que consideres le gusta a tu público"
+        v-model:modelValue="publicPreferences"
+        :validations="[]"
       />
     </div>
   </div>
--- src/views/Forms/CreateBrand/components/CreateBrandStep3.vue
+++ src/views/Forms/CreateBrand/components/CreateBrandStep3.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SelectInput from '@/components/input/SelectInput.vue';
+</script>
 
 <template>
   <div>
-    <h3>Paso 3</h3>
-    <p>Contenido del paso 3</p>
+    <h5>Detalles de industria</h5>
+    <SelectInput
+      label=""/>
   </div>
 </template>
