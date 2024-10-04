68000673ab56323cb4ea74587432b4289be6f6ad
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-10-04 17:18:58 -0500

    ADD: linkedin button, ready to add logic

---
+++ src/components/auth/LinkedInLogin.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+function loginWithLinkedIn() {
+  // TODO: connection
+}
+</script>
+
+<template>
+  <div>
+    <button class="btn btn-linkedin" @click="loginWithLinkedIn">
+      Conectar con LinkedIn
+      <i class="bi bi-linkedin"></i>
+    </button>
+  </div>
+</template>
