065961dbb019559b53d4c11dd119af20ec363b99
Diego Reyes <diegorele13@gmail.com>
2024-10-03 20:07:51 -0500

    add: validator

---
+++ src/validation/components/forms/brand.validation.ts
@@ -0,0 +1,11 @@
+export const nameBrandValidtions = [
+      { rule: (value: string) => value.length > 0, message: 'El nombre es requerido.' },
+      { rule: (value: string) => value.length <= 50, message: 'El nombre no puede exceder 50 caracteres.' }
+    ]
+
+
+
+export const mainAddress = [
+  { rule: (value: string) => value.length > 0, message: 'La dirección es requerida.' },
+  { rule: (value: string) => value.length <= 100, message: 'La dirección no puede exceder 100 caracteres.' }
+]
