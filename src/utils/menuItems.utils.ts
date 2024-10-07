9dd95d760a976c8e5f07a57939f452e0d22d9c00
Diego Reyes <diegorele13@gmail.com>
2024-10-07 18:45:43 -0500

    add: menu items

--- src/utils/menuItems.utils.ts
+++ src/utils/menuItems.utils.ts
@@ -29,11 +29,22 @@ export const menuUserOptions: Navigation[] = [
   }
 ]
 
-export const sidebarItems: Navigation[] = [
-  { name: 'Dashboard', icon: 'bi bi-speedometer', link: 'dashboard' },
-  { name: 'Planificar', icon: 'bi bi-journal-richtext', link: 'planning' },
-  { name: 'Post', icon: 'bi bi-file-post', link: 'post' },
-  { name: 'Tendencias', icon: 'bi bi-bar-chart-line', link: 'trends' },
-  { name: 'Perfil', icon: 'bi bi-person', link: 'profile' },
-  { name: 'Editar marca', icon: 'bi bi-gear-wide-connected', link: 'brand' }
-]
+export const getSidebarItems = (hasBrandSelected: boolean): Navigation[] => {
+  const sidebarItems: Navigation[] = [
+    { name: 'Dashboard', icon: 'bi bi-speedometer', link: 'dashboard' },
+    { name: 'Planificar', icon: 'bi bi-journal-richtext', link: 'planning' },
+    { name: 'Post', icon: 'bi bi-file-post', link: 'post' },
+    { name: 'Tendencias', icon: 'bi bi-bar-chart-line', link: 'trends' },
+    { name: 'Perfil', icon: 'bi bi-person', link: 'profile' }
+  ]
+
+  if (hasBrandSelected) {
+    sidebarItems.push({
+      name: 'Editar marca',
+      icon: 'bi bi-gear-wide-connected',
+      link: 'brand'
+    })
+  }
+
+  return sidebarItems
+}
