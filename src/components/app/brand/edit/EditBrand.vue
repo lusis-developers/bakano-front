f57e5475679fff4e1ae7d867d78274bdff3521e4
Diego Reyes <diegorele13@gmail.com>
2024-10-07 17:49:46 -0500

    add: steps

--- src/components/app/brand/edit/EditBrand.vue
+++ src/components/app/brand/edit/EditBrand.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import CardStep1 from './components/CardStep1.vue'
+import CardStep2 from './components/CardStep2.vue'
+import CardStep3 from './components/CardStep3.vue'
 </script>
 
 <template>
-  <div class="w-75">
+  <div class="w-75 d-flex gap-3">
     <CardStep1 />
+    <CardStep2 />
+    <CardStep3 />
   </div>
 </template>
