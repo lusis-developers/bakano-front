baf14cb90adebf7064dabc0f11612778057a0fd8
Diego Reyes <diegorele13@gmail.com>
2024-10-08 12:26:04 -0500

    fix: scroll

--- src/components/layout/ContainerWrapper.vue
+++ src/components/layout/ContainerWrapper.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container-fluid m-0 p-0 min-vh-100 text-bg-gray-100">
+  <div class="container-fluid m-0 p-0 text-bg-gray-100">
     <div
-      class="container-xxl m-auto d-flex flex-column justify-content-center align-items-center overflow-auto p-4"
+      class="container-xxl m-auto d-flex flex-column justify-content-start align-items-center h-100 overflow-auto p-4"
     >
       <slot name="content" />
     </div>
