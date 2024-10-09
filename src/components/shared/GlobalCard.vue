bc4da86f3a73b3d5a32c1204e3f543fce85469a9
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-10-09 10:18:17 -0500

    REFACTOR: global card

--- src/components/shared/GlobalCard.vue
+++ src/components/shared/GlobalCard.vue
@@ -2,21 +2,19 @@
 defineProps({
   title: {
     type: String,
-    required: true
+    required: false
   }
 })
 </script>
 
 <template>
   <div class="card">
-    <div class="card-header">
-      <h5 class="mb-0">{{ title }}</h5>
-    </div>
     <div class="card-body">
+      <h5 class="card-title">
+        {{ title }}
+      </h5>
+      <hr v-if="title?.length" class="hr" />
       <slot name="content"></slot>
-    </div>
-    <div class="card-footer">
-      <slot name="footer"></slot>
     </div>
   </div>
 </template>
